b690659129fd9287ee645821f9aa7556e4292f15
dreamhugger <nostalgicdreamcast@gmail.com>
2023-10-09 20:26:57 -0300

    Aula 09/10

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function mostrarFormCat(){
+        return view('cad_categoria');
+    }
+
+    public function index(){
+        return view('index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/',[CategoriaController::class,'index'])->name('index');
+Route::get('/cadcategoria',[CategoriaController::class,'mostrarFormCat'])->name('cadcategoria');
